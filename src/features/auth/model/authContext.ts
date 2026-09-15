f0686a3159a7c263bf9b5100d8f29d888bc913a1
romanonyshko501-tech <romanonsyhko501@gmail.com>
2026-09-15 12:18:29 +0200

    create monorepo

--- src/features/auth/model/authContext.ts
+++
@@ -1,9 +0,0 @@
-import type { User } from 'firebase/auth'
-import { createContext } from 'react'
-
-export interface AuthContextValue {
-  user: User | null
-  isLoading: boolean
-}
-
-export const AuthContext = createContext<AuthContextValue | null>(null)
